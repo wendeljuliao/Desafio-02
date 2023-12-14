1cd1d88a83d599614adc50723adc82859c54dbf7
Wendel Julião <wendel.alves@sefaz.ce.gov.br>
2023-12-14 10:28:05 -0300

    desafio feito

---
+++ src/main/java/com/wendel/desafio2/entities/Bloco.java
@@ -0,0 +1,93 @@
+package com.wendel.desafio2.entities;
+
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_bloco")
+public class Bloco {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant inicio;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant fim;
+	
+	@ManyToOne
+	@JoinColumn(name = "atividade_id")
+	private Atividade atividade;
+	
+	public Bloco() {
+	}
+
+	public Bloco(Integer id, Instant inicio, Instant fim, Atividade atividade) {
+		this.id = id;
+		this.inicio = inicio;
+		this.fim = fim;
+		this.atividade = atividade;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Instant getInicio() {
+		return inicio;
+	}
+
+	public void setInicio(Instant inicio) {
+		this.inicio = inicio;
+	}
+
+	public Instant getFim() {
+		return fim;
+	}
+
+	public void setFim(Instant fim) {
+		this.fim = fim;
+	}
+
+	public Atividade getAtividade() {
+		return atividade;
+	}
+
+	public void setAtividade(Atividade atividade) {
+		this.atividade = atividade;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bloco other = (Bloco) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
